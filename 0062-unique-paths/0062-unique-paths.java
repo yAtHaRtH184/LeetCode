ecfc35d7632495dcdd9e6d2a5b1ab937dda12377
Yatharth Kumar Rana <rana.yatharth1804@gmail.com>
2026-07-14 13:06:10 +0530

    Time: 0 ms (100%), Space: 42.2 MB (29.15%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -44,20 +44,41 @@ class Solution {
 
 // tabulation
    
+    // public int uniquePaths(int m, int n) {
+    //     int[][] dp=new int[m][n];
+    //     dp[0][0]=1;
+    //     for(int i=1; i<m; i++){
+    //         dp[i][0]=1;
+    //     }
+    //     for(int j=1; j<n; j++){
+    //         dp[0][j]=1;
+    //     }
+    //     for(int i=1; i<m; i++){
+    //         for(int j=1; j<n; j++){
+    //             dp[i][j]=dp[i-1][j]+dp[i][j-1];
+    //         }
+    //     }
+    //     return dp[m-1][n-1];
+    // }  
+
+// space optmisation
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        dp[0][0]=1;
-        for(int i=1; i<m; i++){
-            dp[i][0]=1;
+        int[] prev=new int[n];
+        
+        for(int j=0; j<n; j++){
+            prev[j]=1;
         }
-        for(int j=1; j<n; j++){
-            dp[0][j]=1;
-        }
+        
         for(int i=1; i<m; i++){
+
+            int[] temp=new int[n];
+            temp[0]=1;
             for(int j=1; j<n; j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                temp[j]=prev[j]+temp[j-1];
             }
+            prev=temp;
         }
-        return dp[m-1][n-1];
+
+        return prev[n-1];
     }  
 }
