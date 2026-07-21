997596f31d0d471ce02428996fbaabcfadad257e
Yatharth Kumar Rana <rana.yatharth1804@gmail.com>
2026-07-21 09:49:36 +0530

    Time: 0 ms (100%), Space: 43.1 MB (11.36%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,24 +1,40 @@
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         int n1=s.length();
+//         int n2=t.length();
+    
+//         if(n1>n2)return false;
+//         int i=0;
+//         int j=0;
+//         while(i<n1 && j<n2){
+//             char ch1=s.charAt(i);
+//             char ch2=t.charAt(j);
+    
+//             if(ch1!=ch2){
+//                 j++;           
+//             } 
+//             else{
+//                 i++;
+//                 j++;
+//             }  
+//         }
+//         if(i==n1)return true;
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int n1=s.length();
-        int n2=t.length();
-    
-        if(n1>n2)return false;
-        int i=0;
-        int j=0;
-        while(i<n1 && j<n2){
-            char ch1=s.charAt(i);
-            char ch2=t.charAt(j);
-    
-            if(ch1!=ch2){
-                j++;           
-            } 
-            else{
-                i++;
-                j++;
-            }  
+        int n=s.length();
+        if(s.isEmpty())return true;
+        int last=0;
+        for(int i=0; i<n; i++){
+            char ch=s.charAt(i);
+            int found=t.indexOf(ch, last);
+            if(found==-1)return false;
+            last=found+1;
         }
-        if(i==n1)return true;
-        return false;
+        return true;
     }
+    
 }
